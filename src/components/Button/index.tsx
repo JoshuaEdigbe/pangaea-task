bb3da5668f20af90410a81962116880696afb014
Joshua Edigbe <joshuaedigbe@gmail.com>
2020-11-27 01:54:02 +0100

    initial components setup

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,11 @@
+// react libraries
+import React from 'react';
+
+// styles
+import './index.scss';
+
+const Button: React.FC = () => {
+  return <button>Button</button>;
+};
+
+export default Button;
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+// react libraries
+import React from 'react';
+
+// components
+import Button from 'components/Button';
+
+const Home: React.FC<any> = () => {
+  return <Button />;
+};
+
+export default Home;
